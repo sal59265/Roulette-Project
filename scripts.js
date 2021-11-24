af223bc3ca5089724ea34e15d95e7b6915b33d02
Soul Lee <lee.soul12@gmail.com>
2021-11-24 14:58:14 -0500

    add dollar, playing and eventlistener

--- scripts.js
+++ scripts.js
@@ -1,12 +1,39 @@
 const cells = document.querySelectorAll('.cell');
 const blackCells = document.querySelectorAll('#black');
 const redCells = document.querySelectorAll('#red');
+const even = document.querySelector('.even');
+const odd = document.querySelector('.odd');
+const one = document.querySelector('.buttonOne');
+const five = document.querySelector('.buttonFive');
+let yourBet = 0;
 let playing = true;
+let currentPlaying = 1;
 
-const bet = (event) => {
+const play = () => {
   if (playing === true) {
-    console.log('hello');
+    yourBet = yourBet + currentPlaying;
   }
 };
 
-cells.addEventlistener('click', alert('hello'));
+const fiveDollar = () => {
+  if (playing === true) {
+    currentPlaying = 5;
+  }
+};
+
+const oneDollar = () => {
+  if (playing === true) {
+    currentPlaying = 1;
+  }
+};
+
+const randomNum = () => {};
+
+cells.forEach((cell) => {
+  cell.addEventListener('click', play);
+});
+// for (let i = 0; i < cells.length; i++) {
+//   cells[i].addEventListener('click', play);
+// }
+five.addEventListener('click', fiveDollar);
+one.addEventListener('click', oneDollar);
