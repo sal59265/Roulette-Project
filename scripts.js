99f99d68a52b1259480db205fb7a5749840f40fc
Soul Lee <lee.soul12@gmail.com>
2021-11-24 13:10:31 -0500

    add even and odd and stylings

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 const cells = document.querySelectorAll('.cell');
 const blackCells = document.querySelectorAll('#black');
 const redCells = document.querySelectorAll('#red');
+let playing = true;
+
+const bet = (event) => {
+  if (playing === true) {
+    console.log('hello');
+  }
+};
+
+cells.addEventlistener('click', alert('hello'));
