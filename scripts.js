23ab01d10e1745feb08c35d798f305eafb5bd193
Soul Lee <lee.soul12@gmail.com>
2021-11-26 23:23:24 -0500

    add button functions and fix problem where the bet was kept made after the play is over

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ const even = document.querySelector('.even');
 const odd = document.querySelector('.odd');
 const one = document.querySelector('.buttonOne');
 const five = document.querySelector('.buttonFive');
+const playMore = document.querySelector('.continue');
+const playAgain = document.querySelector('.restart');
 let bettingAmount = 0;
 let isPlaying = true;
 let currentBettingAmount = 1;
@@ -16,21 +18,21 @@ const play = (event) => {
   if (isPlaying === true) {
     bettingAmount = bettingAmount + currentBettingAmount;
     balance = balance - currentBettingAmount;
-  }
-  document.getElementById(
-    'currentBet'
-  ).innerHTML = `Your Current bet is $${bettingAmount}`;
-  if (bettingAmount >= 30) {
     document.getElementById(
-      'winningNum'
-    ).innerHTML = `The winning number is ${winningNum}!`;
-    document.getElementById(
-      'winning'
-    ).innerHTML = `Your current money is ${balance}`;
-    isPlaying = false;
-  }
-  if (winningNum === parseInt(event.target.innerHTML)) {
-    balance = balance + 34 * currentBettingAmount;
+      'currentBet'
+    ).innerHTML = `Your Current bet is $${bettingAmount}`;
+    if (bettingAmount >= 30) {
+      document.getElementById(
+        'winningNum'
+      ).innerHTML = `The winning number is ${winningNum}!`;
+      document.getElementById(
+        'winning'
+      ).innerHTML = `Your current money is ${balance}`;
+      isPlaying = false;
+    }
+    if (winningNum === parseInt(event.target.innerHTML)) {
+      balance = balance + 34 * currentBettingAmount;
+    }
   }
 };
 
@@ -55,11 +57,29 @@ const toggleBettingAmount = () => {
 };
 
 cells.forEach((cell) => {
-  cell.addEventListener('click', play);
+  if (isPlaying === true) {
+    cell.addEventListener('click', play);
+  }
 });
 
-// for (let i = 0; i < cells.length; i++) {
-//   cells[i].addEventListener('click', play);
-// }
+const continuePlaying = () => {
+  isPlaying = true;
+  currentBettingAmount = 1;
+  bettingAmount = 0;
+  alert('Keep playing');
+};
+
+const againPlaying = () => {
+  isPlaying = true;
+  currentBettingAmount = 1;
+  bettingAmount = 0;
+  balance = 100;
+  document.getElementById(
+    'winning'
+  ).innerHTML = `Your current money is ${balance}`;
+  alert('Better luck this time');
+};
 five.addEventListener('click', toggleBettingAmount);
 one.addEventListener('click', toggleBettingAmount);
+playMore.addEventListener('click', continuePlaying);
+playAgain.addEventListener('click', againPlaying);
